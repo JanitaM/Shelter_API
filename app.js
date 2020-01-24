a8e4006062334b68bf0f325c654463c4b961a340
Janita M <48458664+JanitaM@users.noreply.github.com>
2020-01-23 21:21:59 -0500

    added array of dummy dog data. updated js to display dummy data on website.

--- app.js
+++ app.js
@@ -1,42 +1,109 @@
+// Dummy Dog Data
+const dogList = [
+  {
+    Name: 'Fenway',
+    ID: '1234',
+    Age: '5',
+    Gender: 'Male',
+    intake: '9/10/2019'
+  },
+  {
+    name: 'Folly',
+    id: '1235',
+    age: '1',
+    gender: 'Female',
+    intake: '12/20/2019'
+  },
+  {
+    name: 'Whiskey',
+    id: '1236',
+    age: '2',
+    gender: 'Female',
+    intake: '1/6/2020'
+  }
+];
+
 // Variables
-let
+const
   images = document.getElementById('card-img'),
   breedBtn = document.getElementById('breedBtn'),
   randomBtn = document.getElementById('randomBtn'),
   display = document.getElementById('display-container'),
-  breed = document.getElementById('breedValue');;
+  breed = document.getElementById('breedValue'),
+  leftCol = document.getElementById('card-left-col'),
+  rightCol = document.getElementById('card-right-col');
 
 // Functions
+// Get API Request
 const getRequest = e => {
   if (e.target.id === `breedBtn`) {
     url = `https://dog.ceo/api/breed/${breed.value}/images`;
-
     $.getJSON(url, function (response) {
-      parseBreedJSON(response);
+      return parseBreedJSON(response);
     });
   } else if (e.target.id === `randomBtn`) {
     $.getJSON(`https://dog.ceo/api/breeds/image/random`, function (response) {
-      parseRandomJSON(response);
+      return parseRandomJSON(response);
     });
   }
 
   e.preventDefault();
 }
 
+// Specific Breed Request
 const parseBreedJSON = json => {
-  let index = Math.floor(Math.random() * json.message.length);
-  images.src = json.message[index];
+  let randomIndex = Math.floor(Math.random() * json.message.length);
+  images.src = json.message[randomIndex];
   display.style.display = 'block';
   breed.value = "";
+  populateData();
 }
 
+// Random Breed Request
 const parseRandomJSON = json => {
   images.src = json.message;
   display.style.display = 'block';
+  populateData();
 }
+
+// Add Dog Data
+const populateData = (e) => {
+  // Create a new ul
+  const ulLeft = document.createElement('ul');
+  const ulRight = document.createElement('ul');
+
+  // Create random index
+  let randomIndex = Math.floor(Math.random() * dogList.length);
+  // Pull random data from the dogList array
+  let randomData = dogList[randomIndex];
+  // Print the data
+  const data = Object.entries(randomData);
+
+  // Left Col
+  for (let [key, value] of data) {
+    let li = document.createElement('li');
+    ulLeft.appendChild(li);
+    li.innerHTML += `${key.toUpperCase()}:`;
+  }
+  // append ul to left col
+  leftCol.appendChild(ulLeft);
+
+  // Right Col
+  for (let [key, value] of data) {
+    let li = document.createElement('li');
+    ulRight.appendChild(li);
+    li.innerHTML += value.toUpperCase();
+  }
+  // append ul to right col
+  rightCol.appendChild(ulRight);
+}
+
+// References
+
+// https://zellwk.com/blog/looping-through-js-objects/
+// https://www.vitoshacademy.com/javascript-create-ul-and-li-elements-with-js-with-chained-function/
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
 
 // Event Listeners
 breedBtn.addEventListener('click', getRequest);
 randomBtn.addEventListener('click', getRequest);
-
-// A cookie associated with a cross-site resource at http://dog.ceo/ was set without the `SameSite` attribute. A future release of Chrome will only deliver cookies with cross-site requests if they are set with `SameSite=None` and `Secure`. You can review cookies in developer tools under Application>Storage>Cookies and see more details at https://www.chromestatus.com/feature/5088147346030592 and https://www.chromestatus.com/feature/5633521622188032.
