33c2a708eb45abc55373f6c2f0cb265fd747e538
Janita M <48458664+JanitaM@users.noreply.github.com>
2020-01-25 13:12:38 -0500

    updated JS to display 3 dogs by updating html

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Dummy Dog Data
 const dogList = [
   {
-    Name: 'Fenway',
-    ID: '1234',
-    Age: '5',
-    Gender: 'Male',
+    name: 'Fenway',
+    id: '1234',
+    age: '5',
+    gender: 'Male',
     intake: '9/10/2019'
   },
   {
@@ -31,28 +31,24 @@ const dogList = [
 ];
 
 // Variables
-const
-  images = document.getElementById('card-img'),
+const  //const or let??
+  image = document.querySelector('card-img'),
   breedBtn = document.getElementById('breedBtn'),
   randomBtn = document.getElementById('randomBtn'),
   display = document.getElementById('display-container'),
-  breed = document.getElementById('breedValue'),
-  leftCol = document.getElementById('card-left-col'),
-  rightCol = document.getElementById('card-right-col');
+  breed = document.getElementById('breedValue');
 
 // Functions
 // Get API Request
 const getRequest = e => {
   if (e.target.id === `breedBtn`) {
-    url = `https://dog.ceo/api/breed/${breed.value}/images`;
+    url = `https://dog.ceo/api/breed/${breed.value}/images`;  //returns an array of many dogs
     $.getJSON(url, function (response) {
       parseBreedJSON(response);
-      populateData();
     });
   } else if (e.target.id === `randomBtn`) {
-    $.getJSON(`https://dog.ceo/api/breeds/image/random`, function (response) {
+    $.getJSON(`https://dog.ceo/api/breeds/image/random/3`, function (response) { //returns array of 3 dogs
       parseRandomJSON(response);
-      populateData();
     });
   }
 
@@ -61,54 +57,80 @@ const getRequest = e => {
 
 // Specific Breed Request
 const parseBreedJSON = json => {
-  let randomIndex = Math.floor(Math.random() * json.message.length);
-  images.src = json.message[randomIndex];
-  display.style.display = 'block';
-  breed.value = "";
+  let randomIndex = Math.floor(Math.random() * json.message.length);  //create random index
+  let dogsArray = json.message.slice(randomIndex, randomIndex + 3);  //save random index plus 2 additional urls to an array
+  populateData(dogsArray);  //passes in image urls
+  display.style.display = 'block';  //show display-container
+  breed.value = "";  //clear input field
 }
 
 // Random Breed Request
 const parseRandomJSON = json => {
-  images.src = json.message;
-  display.style.display = 'block';
+  let dogsArray = json.message; //save 3 elements to array
+  populateData(dogsArray);  //passes in images
+  display.style.display = 'block';  //show display-container
 }
 
-// Add Dog Data
-const populateData = () => {
+// Add Dog Images and Data
+const populateData = arr => {  //takes in dogsArray
   // Clear existing data first
-  if (leftCol.firstChild && rightCol) {
-    leftCol.removeChild(leftCol.firstChild);
-    rightCol.removeChild(rightCol.firstChild);
+  while (display.hasChildNodes()) {
+    display.removeChild(display.firstChild);
   }
 
-  // Create a new ul
-  const ulLeft = document.createElement('ul');
-  const ulRight = document.createElement('ul');
+  arr.forEach(el => {  //for each element in array
+    const newCardGroup = document.createElement('div');  //create new card-group div
+    newCardGroup.className = 'card-group';  //add class of card-group
+    const newImg = document.createElement('img');  //create a new img tag
+    newImg.src = el;  //set src to el
+    newCardGroup.appendChild(newImg);  //append to newCardGroup
 
-  // Create random index
-  let randomIndex = Math.floor(Math.random() * dogList.length);
-  // Pull random data from the dogList array
-  let randomData = dogList[randomIndex];
-  // Print the data
-  const data = Object.entries(randomData);
+    const newCardInfo = document.createElement('div');  //create new card-info div
+    newCardInfo.className = 'card-info';  //add class of card-info
+    newCardGroup.appendChild(newCardInfo);  //append to newCardGroup
 
-  // Left Col
-  for (let [key, value] of data) {
-    let li = document.createElement('li');
-    ulLeft.appendChild(li);
-    li.innerHTML += `${key.toUpperCase()}:`;
-  }
-  // append ul to left col
-  leftCol.appendChild(ulLeft);
+    // Create card-left-col and card-right-col
+    const newLeftCol = document.createElement('div');
+    newLeftCol.className = 'card-left-col';
 
-  // Right Col
-  for (let [key, value] of data) {
-    let li = document.createElement('li');
-    ulRight.appendChild(li);
-    li.innerHTML += value.toUpperCase();
-  }
-  // append ul to right col
-  rightCol.appendChild(ulRight);
+    const newRightCol = document.createElement('div');
+    newRightCol.className = 'card-right-col';
+
+    // Append both to card-info
+    newCardInfo.appendChild(newLeftCol);
+    newCardInfo.appendChild(newRightCol);
+
+    // Create left and right ul's
+    const ulLeft = document.createElement('ul');
+    const ulRight = document.createElement('ul');
+
+    // Create random index
+    let randomIndex = Math.floor(Math.random() * dogList.length);
+    // Pull random data from the dogList array
+    let randomData = dogList[randomIndex];
+    // Print the data
+    const data = Object.entries(randomData);
+
+    // Left Col - loop through dog object at randomIndex
+    for (let [key, value] of data) {
+      let li = document.createElement('li');  //create a new li
+      ulLeft.appendChild(li);  //append to ul
+      li.innerHTML += `${key.toUpperCase()}:`;  //save the key as li, convert to uppercase
+    }
+
+    newLeftCol.appendChild(ulLeft);  //append ul to left col
+
+    // Right Col - loop through dog object at randomIndex
+    for (let [key, value] of data) {
+      let li = document.createElement('li');  //create a new li
+      ulRight.appendChild(li);  //append to ul
+      li.innerHTML += value.toUpperCase();  //save the value as li, convert to uppercase
+    }
+
+    newRightCol.appendChild(ulRight);  //append ul to right col
+
+    display.appendChild(newCardGroup)  // Append to card-group to display-container
+  });
 }
 
 // Event Listeners
